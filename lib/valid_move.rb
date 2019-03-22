52b351b278558e27daacbc00c5190307bc2cc48f
Uriel Eleazar Tejeiro Garcia <eleazartejeiro@gmail.com>
2019-03-22 03:48:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,16 @@
 # code your #valid_move? method here
-def valid_move?
+def valid_move?(tablero, indice)
+  if position_taken?(tablero, indice)
+    return true
+  else
+    return false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken? (board, index)
     if board != nil && index != nil
       if board[index] == "" || board[index] == " " || board[index] == nil
-        return false
-      else
         return true
       end
     end
